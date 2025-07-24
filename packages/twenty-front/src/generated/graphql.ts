15013f2af3acc1bce9f18c3d220250fa512c91c1
martmull <martmull@hotmail.fr>
2025-07-24 09:52:00 +0200

    Add position in step definition

--- packages/twenty-front/src/generated/graphql.ts
+++ packages/twenty-front/src/generated/graphql.ts
@@ -2867,6 +2867,7 @@ export type WorkflowAction = {
   id: Scalars['UUID'];
   name: Scalars['String'];
   nextStepIds?: Maybe<Array<Scalars['UUID']>>;
+  position?: Maybe<WorkflowStepPosition>;
   settings: Scalars['JSON'];
   type: Scalars['String'];
   valid: Scalars['Boolean'];
@@ -2881,6 +2882,12 @@ export type WorkflowEdge = {
 export type WorkflowRun = {
   __typename?: 'WorkflowRun';
   workflowRunId: Scalars['UUID'];
+};
+
+export type WorkflowStepPosition = {
+  __typename?: 'WorkflowStepPosition';
+  x: Scalars['Float'];
+  y: Scalars['Float'];
 };
 
 export type WorkflowVersion = {
