8a59365c194cba9af109d9961e39020411422f99
martmull <martmull@hotmail.fr>
2025-07-27 22:16:07 +0200

    Add tidyup button

--- packages/twenty-front/src/generated/graphql.ts
+++ packages/twenty-front/src/generated/graphql.ts
@@ -1181,6 +1181,7 @@ export type Mutation = {
   trackAnalytics: Analytics;
   updateApiKey?: Maybe<ApiKey>;
   updateDatabaseConfigVariable: Scalars['Boolean'];
+  updateDraftWorkflowVersionPositions: Scalars['Boolean'];
   updateLabPublicFeatureFlag: FeatureFlagDto;
   updateOneAgent: Agent;
   updateOneField: Field;
@@ -1590,6 +1591,11 @@ export type MutationUpdateApiKeyArgs = {
 export type MutationUpdateDatabaseConfigVariableArgs = {
   key: Scalars['String'];
   value: Scalars['JSON'];
+};
+
+
+export type MutationUpdateDraftWorkflowVersionPositionsArgs = {
+  input: UpdateDraftWorkflowVersionPositionsInput;
 };
 
 
@@ -2611,6 +2617,13 @@ export type UpdateApiKeyDto = {
   revokedAt?: InputMaybe<Scalars['String']>;
 };
 
+export type UpdateDraftWorkflowVersionPositionsInput = {
+  /** Workflow version updated positions */
+  positions: Array<WorkflowStepPositionUpdateInput>;
+  /** Workflow version ID */
+  workflowVersionId: Scalars['String'];
+};
+
 export type UpdateFieldInput = {
   defaultValue?: InputMaybe<Scalars['JSON']>;
   description?: InputMaybe<Scalars['String']>;
@@ -2885,6 +2898,17 @@ export type WorkflowStepPosition = {
   __typename?: 'WorkflowStepPosition';
   x: Scalars['Float'];
   y: Scalars['Float'];
+};
+
+export type WorkflowStepPositionInput = {
+  x: Scalars['Float'];
+  y: Scalars['Float'];
+};
+
+export type WorkflowStepPositionUpdateInput = {
+  /** Step or trigger ID */
+  id: Scalars['String'];
+  position: WorkflowStepPositionInput;
 };
 
 export type WorkflowVersion = {
